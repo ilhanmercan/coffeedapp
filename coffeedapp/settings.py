a8b829d3a33c24f0f62f3f2ca3a23dfaa345d0f1
Ilhan Mercan <ilhanmercan@hotmail.com>
2015-10-15 00:15:12 +0300

    Basic blog is working!

--- coffeedapp/settings.py
+++ coffeedapp/settings.py
@@ -97,7 +97,7 @@ else:
 
             'ENGINE': 'django.db.backends.sqlite3',
 
-            'NAME': os.path.join(MAIN_DIR, 'db.sqlite3'),
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
         }
 
